b2b284985e7e2c910633724bd2af70a06548e370
Adam Green <greena88@gmail.com>
2011-10-24 19:49:38 +0100

    Fix build fail due to CMScreenshot update

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1827,6 +1827,9 @@ public final class Settings {
          */
         public static final String BATTERY_PERCENTAGE = "battery_percentage";
 
+        /** @hide */
+        public static final String SHARE_SCREENSHOT = "share_screenshot";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
