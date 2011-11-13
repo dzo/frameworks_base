b9e5bfe1714d004b779c154bd7b7a3a608a3aa11
Adam Green <greena88@gmail.com>
2011-11-13 03:44:37 +0000

    Hide indic. by default

--- packages/SystemUI/src/com/android/systemui/statusbar/powerwidget/PowerButton.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/powerwidget/PowerButton.java
@@ -102,7 +102,7 @@ public abstract class PowerButton {
                     int buttonState = R.id.power_widget_button_indic;
 
                     ImageView indic = (ImageView)mView.findViewById(R.id.power_widget_button_indic);
-                    if ((Settings.System.getInt(context.getContentResolver(),Settings.System.EXPANDED_HIDE_INDICATOR, 0)) == 1){
+                    if ((Settings.System.getInt(context.getContentResolver(),Settings.System.EXPANDED_HIDE_INDICATOR, 1)) == 1){
                         indic.setVisibility(8);
                     }else{
                         indic.setVisibility(0);
