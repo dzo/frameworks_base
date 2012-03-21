42f8858c28ed175ae5712825c974714c71846e4c
Avss Srikanth <srikan@codeaurora.org>
2012-03-21 11:23:33 -0700

    libstagefright: Reset buffering event pending flag after update to app
    -When buffering even pending flag is set before the cached duration is
     calculated and sent to app causes another buffering event being
     processed before the first one finishes.
    -Reset the buffering event pending flag only after update to app
    
    CRs-Fixed: 343726
    (cherry picked from commit c1dd5c94a08f21a905f11e7bdc921329e9752ba9)
    
    Change-Id: Idc4a79aaa8ca53b3adab70d00eca8678f0f751a7

--- media/libstagefright/AwesomePlayer.cpp
+++ media/libstagefright/AwesomePlayer.cpp
@@ -698,7 +698,6 @@ void AwesomePlayer::onBufferingUpdate() {
     if (!mBufferingEventPending) {
         return;
     }
-    mBufferingEventPending = false;
     bool cacheFull = false;
 
     if (mCachedSource != NULL) {
@@ -811,6 +810,7 @@ void AwesomePlayer::onBufferingUpdate() {
         }
     }
 
+    mBufferingEventPending = false;
     postBufferingEvent_l();
 }
 
