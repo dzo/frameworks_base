e73d0d4b9089d719e39efbc444cd519bb07092f3
Ravishankar Sarawadi <crsaraw@codeaurora.org>
2012-03-22 11:46:16 -0700

    LPAPlayerALSA: Fix for acquire/release Wake-locks.
     -Requires a wake lock to ensure that 3s timer after playback runs
     in suspend mode to get into TCXO shutdown.
     -Add support for acquiring wake locks from mediaserver process
     -PowerService is used for acquiring/releasing wakelock
    
    Change-Id: Icb21c319eee24aa38d56afcd8eddcb6315b74558
    CRs-Fixed: 338542

--- media/libstagefright/Android.mk
+++ media/libstagefright/Android.mk
@@ -109,6 +109,7 @@ LOCAL_SHARED_LIBRARIES := \
         libssl           \
         libgui           \
         libhardware_legacy \
+        libpowermanager
 
 LOCAL_STATIC_LIBRARIES := \
         libstagefright_color_conversion \
